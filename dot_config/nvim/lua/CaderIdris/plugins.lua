366d57e39ea2be4487bbec2a176f1b6bf10c28ff
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2023-05-20 16:13:46 +0100

    Converted from Packer to plogins

--- dot_config/nvim/lua/CaderIdris/plugins.lua
+++ dot_config/nvim/lua/CaderIdris/plugins.lua
@@ -1,143 +1,189 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local function manage_plugins()
+    local plugins = {
+        ["https://github.com/faerryn/plogins.nvim.git"] = {},
+	["https://github.com/nvim-lua/plenary.nvim.git"] = {},
+	-- Required by some plugins for symbols
+	["https://github.com/kyazdani42/nvim-web-devicons.git"] = {},
+
+	-- Fidget. Shows lsp progress
+	["https://github.com/j-hui/fidget.nvim.git"] = {
+		packadd_hook = function()
+			require('fidget').setup()
+		end,
+	},
+
+	-- Colour things in
+	["https://github.com/norcalli/nvim-colorizer.lua.git"] = {
+		packadd_hook = function()
+			require('colorizer').setup()
+		end,
+	},
+
+	-- File tree
+	["https://github.com/kyazdani42/nvim-tree.lua.git"] = {
+		packadd_hook = function()
+			require('nvim-tree').setup()
+		end,
+	},
+
+	--  Telescope
+	["https://github.com/nvim-telescope/telescope.nvim"] = {},
+	["https://github.com/nvim-telescope/telescope-fzf-native.nvim"] = {},
+	["https://github.com/nvim-telescope/telescope-symbols.nvim"] = {},
+	["https://github.com/karb94/neoscroll.nvim"] = {},
+
+	-- More surround commands
+	["https://github.com/tpope/vim-repeat"] = {},
+	["https://github.com/tpope/vim-surround"] = {},
+
+	-- More word objects
+	["https://github.com/AndrewRadev/splitjoin.vim"] = {},
+
+	-- Autopairs
+	["https://github.com/windwp/nvim-autopairs"] = {},
+
+	-- Tagbar
+	["https://github.com/preservim/tagbar"] = {},
+
+	-- EasyNavigator replacement
+	["https://github.com/phaazon/hop.nvim.git"] = {
+		packadd_hook = function()
+			require('hop').setup()
+		end,
+	},
+
+
+        ["https://github.com/nvim-lualine/lualine.nvim.git"] = {
+		packadd_hook = function()
+			require("lualine").setup {
+				options = {
+					icons_enabled = true,
+					component_separators = { left = "", right = "" },
+					section_separators   = { left = "", right = "" },
+					theme = 'gruvbox_dark' 
+				},
+			    tabline = { lualine_a = { "tabs" } },
+			}
+		    end,
+        },
+
+        ["https://github.com/lewis6991/gitsigns.nvim.git"] = {
+            packadd_hook = function()
+                require("gitsigns").setup()
+            end,
+        },
+
+        ["https://github.com/nvim-treesitter/nvim-treesitter.git"] = {
+            packadd_hook = function()
+                require("nvim-treesitter.configs").setup({
+                    ensure_installed = "all",
+                    highlight = { enable = true },
+                    indent    = { enable = true },
+                })
+                vim.opt.foldmethod = "expr"
+                vim.opt.foldexpr   = "nvim_treesitter#foldexpr()"
+            end,
+            upgrade_hook = function()
+                require("nvim-treesitter.install").update()
+            end,
+        },
+
+        ["https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git"] = {
+            packadd_after = { ["https://github.com/nvim-treesitter/nvim-treesitter.git"] = true },
+            packadd_hook = function()
+                require("nvim-treesitter.configs").setup({
+                    textobjects = {
+                        select = {
+                            enable = true,
+                            lookahead = true,
+                            keymaps = {
+                                ["af"] = "@function.outer",
+                                ["if"] = "@function.inner",
+                                ["ac"] = "@class.outer",
+                                ["ic"] = "@class.inner",
+                            },
+                        },
+                    },
+                })
+            end,
+        },
+
+	["https://github.com/junegunn/vim-emoji.git"] = {},
+	-- Theme
+	["https://github.com/ellisonleao/gruvbox.nvim.git"] = {
+        	packadd_after = { ["https://github.com/norcalli/nvim-colorizer.lua.git"] = true },
+		packadd_hook = function()
+			require("gruvbox").setup()
+			vim.cmd('set background=dark')
+			vim.cmd('colorscheme gruvbox')
+		end,
+	},
+
+	-- LSP
+	["https://github.com/williamboman/mason.nvim.git"] = {
+		packadd_hook = function()
+			require("mason").setup()
+		end,
+	},
+
+	["https://github.com/williamboman/mason-lspconfig.nvim.git"] = {
+        	packadd_after = { ["https://github.com/williamboman/mason.nvim.git"] = true },
+		packadd_hook = function()
+			require("mason-lspconfig").setup {
+				ensure_installed = {
+					'bashls',
+					'cssls',
+					'html',
+					'jsonls',
+					'arduino_language_server',
+					'clangd',
+					'eslint',
+					'jedi_language_server',
+					'marksman',
+					'rust_analyzer',
+					'lua_ls',
+					'texlab',
+					'yamlls',
+					'pyright',
+					'taplo',
+					'esbonio',
+					'dockerls' 
+				}
+			}
+		end,
+	},
+
+	["https://github.com/neovim/nvim-lspconfig.git"] = {
+        	packadd_after = { ["https://github.com/williamboman/mason-lspconfig.nvim.git"] = true },
+		packadd_hook = function()
+			require("lspconfig").setup()
+		end,
+	},
+
+	["https://github.com/ms-jpq/coq_nvim.git"] = {},
+	["https://github.com/ms-jpq/coq.artifacts.git"] = {},
+	["https://github.com/ms-jpq/coq.thirdparty.git"] = {},
+
+	["https://github.com/dense-analysis/ale.git"] = {},
+    }
+
+    local manager = require("plogins").manage(plugins)
+
+    vim.api.nvim_create_user_command("PloginsUpgrade",    manager.upgrade,    {})
+    vim.api.nvim_create_user_command("PloginsAutoremove", manager.autoremove, {})
 end
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup({
-	function(use)
-		use 'wbthomason/packer.nvim'
-
-		-- Required for telescope and gitsigns
-		use 'nvim-lua/plenary.nvim'
-
-		-- Required by some plugins for symbols
-		use 'kyazdani42/nvim-web-devicons'
-
-		-- Statusbar
-		use 'nvim-lualine/lualine.nvim'
-		require('lualine').setup {
-			options = { theme = 'gruvbox_dark' }
-		}
-
-		-- Fidget. Shows lsp progress
-		use 'j-hui/fidget.nvim'
-		require('fidget').setup()
-
-		-- Treesitter
-		use {
-			'nvim-treesitter/nvim-treesitter',
-			run = function()
-				require('nvim-treesitter.install').update({ with_sync = true })
-			end,
-		}
-		use 'p00f/nvim-ts-rainbow'
-		require('nvim-treesitter.configs').setup {
-			ensure_installed = {"python", "lua", "rust", "bash", "markdown", "json", "bibtex", "latex", "html", "yaml", "toml"},
-			rainbow = {
-				enable = true,
-				extended_mode = true,
-				max_file_lines = nil
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-		}
-
-
-		-- Colour things in
-		use 'norcalli/nvim-colorizer.lua'
-		require('colorizer').setup()
-
-		-- Gitsigns
-		use 'lewis6991/gitsigns.nvim'
-		require('gitsigns').setup()
-
-		-- File tree
-		use 'kyazdani42/nvim-tree.lua'
-		require('nvim-tree').setup()
-
-		--  Telescope
-		use 'nvim-telescope/telescope.nvim'
-		use 'nvim-telescope/telescope-fzf-native.nvim'
-		use 'nvim-telescope/telescope-symbols.nvim'
-
-		-- EasyNavigator replacement
-		use 'phaazon/hop.nvim'
-		require('hop').setup()
-
-		-- Better scrolling
-		use 'karb94/neoscroll.nvim'
-
-		-- More surround commands
-		use 'tpope/vim-repeat'
-		use 'tpope/vim-surround'
-
-		-- More word objects
-		use 'AndrewRadev/splitjoin.vim'
-
-		-- Autopairs
-		use 'windwp/nvim-autopairs'
-
-		-- Tagbar
-		use 'preservim/tagbar'
-
-		-- Jupyter integration 
-		use 'untitled-ai/jupyter_ascending.vim'
-		vim.g.jupyter_ascending_python_executable = 'python3'
-
-		-- Emojis
-		use 'junegunn/vim-emoji'
-
-		-- Markdown Preview
-		use 'euclio/vim-markdown-composer'
-		vim.g.markdown_composer_port = '8001'
-
-		-- Theme
-		use 'ellisonleao/gruvbox.nvim'
-		require("gruvbox").setup {
-			undercurl = true,
-			underline = true,
-			bold = true,
-			italic = false,
-		}
-		vim.cmd('set background=dark')
-		vim.cmd('colorscheme gruvbox')
-
-		-- LSP
-		use 'neovim/nvim-lspconfig'
-		local lspconfig = require('lspconfig')
-		use 'williamboman/nvim-lsp-installer'
-		require('nvim-lsp-installer').setup({ automatic_installation = true })
-		use 'ms-jpq/coq_nvim'
-		use 'ms-jpq/coq.artifacts'
-		use 'ms-jpq/coq.thirdparty'
-
-		use 'dense-analysis/ale'
-
-
-
-		vim.g.coq_settings = { auto_start = true }
-
-		local server = { 'bashls', 'cssls', 'html', 'jsonls', 'arduino_language_server', 'clangd', 'eslint', 'jedi_language_server', 'marksman', 'rust_analyzer', 'sumneko_lua', 'texlab', 'yamlls', 'pyright', 'taplo', 'r_language_server', 'esbonio', 'dockerls' }
-		for _, lsp in ipairs(server) do
-			lspconfig[lsp].setup(require('coq').lsp_ensure_capabilities({}))
-		end
-	end,
-
-	config = {
-		display = {
-			open_fn = function()
-				return require('packer.util').float({ border = 'single' })
-			end,
-			},
-		},
-})
+local plogins_source = "https://github.com/faerryn/plogins.nvim.git"
+local plogins_name = plogins_source:gsub("/", "%%")
+local plogins_dir = ("%s/site/pack/plogins/opt/%s"):format((vim.fn.stdpath("data")), plogins_name)
+if not vim.loop.fs_stat(plogins_dir) then
+    vim.loop.spawn("git", { args = { "clone", "--depth", "1", plogins_source, plogins_dir } }, function(code, signal)
+        vim.defer_fn(function()
+            vim.cmd(("packadd %s"):format(vim.fn.fnameescape(plogins_name)))
+            manage_plugins()
+        end, 0)
+    end)
+else
+    vim.cmd(("packadd %s"):format(vim.fn.fnameescape(plogins_name)))
+    manage_plugins()
+end
