67fde9b9c543d5c78fe72c7e57a25a7b9acb2f17
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2023-07-25 09:02:03 +0100

    Changed zsh plugin manager to miniplug and added quote module, currently only has quotes from The Plague

---
+++ dot_local/share/quotes/run.zsh
@@ -0,0 +1,5 @@
+quote_raw=$(shuf -n 1 ~/.local/share/quotes/quotes.txt)
+IFS='|' read -rA quote_array <<< $quote_raw
+
+quote_string="\"$quote_array[1]\" - \e[3m$quote_array[2]\e[0m"
+echo $quote_string
