08f958d2569976bbdfda749a63421ef2ce76010a
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2023-08-28 14:38:23 +0100

    Updated quotes and added new dependencies to pipfile

--- dot_local/share/quotes/run.zsh
+++ dot_local/share/quotes/run.zsh
@@ -1,5 +1,7 @@
+#!/bin/zsh
+
 quote_raw=$(shuf -n 1 ~/.local/share/quotes/quotes.txt)
 IFS='|' read -rA quote_array <<< $quote_raw
 
 quote_string="\"$quote_array[1]\" - \e[3m$quote_array[2]\e[0m"
-echo $quote_string
+echo $quote_string | lolcat -t
