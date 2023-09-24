08742eb06196ca20e76c05ce3d65086a23257f42
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2023-09-24 14:38:43 +0100

    Added wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.warn_about_missing_glyphs = false
+
+config.window_close_confirmation = 'NeverPrompt'
+
+return config
