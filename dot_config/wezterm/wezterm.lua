b5f867fbaf016c5cd8a5ae3cce91e958dcf02e6b
CaderIdris <61825162+Joppleganger@users.noreply.github.com>
2023-11-02 11:36:45 +0000

    Light changes to prompt

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,4 +6,11 @@ config.warn_about_missing_glyphs = false
 
 config.window_close_confirmation = 'NeverPrompt'
 
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0
+}
+
 return config
--- dot_local/share/quotes/run.zsh
+++ dot_local/share/quotes/run.zsh
@@ -3,5 +3,5 @@
 quote_raw=$(shuf -n 1 ~/.local/share/quotes/quotes.txt)
 IFS='|' read -rA quote_array <<< $quote_raw
 
-quote_string="\"$quote_array[1]\" - \e[3m$quote_array[2]\e[0m"
+quote_string="\033[1;35m\"$quote_array[1]\"\033[0m - \e[3m$quote_array[2]\e[0m"
 echo $quote_string
